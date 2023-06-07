aa83942635730f9988fabadf723475d3dab58d05
ivanLFN <LFN.current@yandex.com>
2023-06-07 04:15:25 -0700

    Added gallary element

--- soh_web/frontend/views.py
+++ soh_web/frontend/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from frontend.models import Partnership
+from frontend.models import Partnership, Image
 
 def main_app(request):
     partners_list = Partnership.objects.all()
+    available_images = Image.objects.filter(type_img='gallary_type')
     data = {
-        'partners_list': partners_list
+        'partners_list': partners_list,
+        'available_images': available_images
     }
     return render(request, 'main_app.html', data)
 
 
 def partner_detail(request, partner_title):
     partner = get_object_or_404(Partnership, title=partner_title)
-    return render(request, 'general_elements/partner_detail.html', {'partner': partner})
+    segments_of_work = list(partner.segmets_of_work.all())
+
+    tabs = []
+    for segment in segments_of_work:
+        tab = {
+            'id': f'tab{segment.pk}',
+            'title': segment.title,
+            'content': segment.about,
+            'image': segment.image,
+            'active': False, 
+        }
+        tabs.append(tab)
+
+    if tabs:
+        tabs[0]['active'] = True
+
+    return render(request, 'general_elements/partner_detail.html', {'partner': partner, 'tabs': tabs})
+
+    
+
+
+
+
