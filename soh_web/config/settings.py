6c16d54762a08180b9ecd38a97111601816f20e9
ivanLFN <LFN.current@yandex.com>
2023-06-08 06:34:24 -0700

    Made settings for deploy

--- soh_web/config/settings.py
+++ soh_web/config/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+25&zhibsr$lfe7y+^&4mqips6(v%o_txah3shl**n=oac_22f'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-+25&zhibsr$lfe7y+^&4mqips6(v%o_txah3shl**n=oac_22f')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
